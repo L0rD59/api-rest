d90361a61501c6f453c0770c3fcc89cf0e856832
JBHome <johannbrocail@enaco.fr>
2014-03-22 20:02:44 +0100

    commit

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -8,6 +8,10 @@ class WelcomeController extends Controller
 {
     public function indexAction()
     {
+        $request = new \HttpRequest('http://api-rest.local/web/app_dev.php/demo/users');
+        $request->addHeaders(array('x-wsse' => 'UsernameToken Username="test", PasswordDigest="test", Nonce="test", Created="2014-01-16"'));
+
+        var_dump($request->send()->getBody());
         /*
          * The action's view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
