f076f250bea2f5e0f5e8f5c1c274224152ccbd10
JBHome <johannbrocail@enaco.fr>
2014-03-24 20:34:12 +0100

    add vagrant

--- src/Acme/DemoBundle/Controller/WelcomeController.php
+++ src/Acme/DemoBundle/Controller/WelcomeController.php
@@ -8,11 +8,11 @@ class WelcomeController extends Controller
 {
     public function indexAction()
     {
-        echo base64_encode(sha1(base64_decode('1') . '2014-01-23 11:40:00' . 'userpass', true));
-        $request = new \HttpRequest('http://api-rest.local/web/app_dev.php/demo/users');
+        echo base64_encode(sha1(base64_decode('1') . '2014-03-22 21:15:00' . 'userpass', true));
+        /*$request = new \HttpRequest('http://api-rest.local/web/app_dev.php/demo/users');
         $request->addHeaders(array('x-wsse' => 'UsernameToken Username="test", PasswordDigest="test", Nonce="test", Created="2014-01-16"'));
 
-        var_dump($request->send()->getBody());
+        var_dump($request->send()->getBody());*/
         /*
          * The action's view can be rendered using render() method
          * or @Template annotation as demonstrated in DemoController.
